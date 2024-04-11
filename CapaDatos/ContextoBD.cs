9b78eccb189ff7711f5ef84d131a21dea6b24829
AliciaGuz <gl0214032023@unab.edu.sv>
2024-04-11 11:25:04 -0600

    Actualización de Proyecto

---
+++ CapaDatos/ContextoBD.cs
@@ -0,0 +1,26 @@
+﻿using CapaEntidades;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CapaDatos
+{
+    public partial class ContextoBD : DbContext
+    {
+        public ContextoBD()
+            : base("name=ContextoBD")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+        public virtual DbSet<Ventas> Ventas { get; set; }
+        public virtual DbSet<Producto> Productos { get; set; }
+        public virtual DbSet<DetalleVenta> DetalleVentas { get; set; }
+
+    }
+}
