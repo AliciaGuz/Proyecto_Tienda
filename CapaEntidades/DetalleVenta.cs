98c8e62de10f765d72d11ca44d3208aaefc8fe18
Mauricio007 <va0028032023@unab.edu.sv>
2024-04-11 12:03:04 -0600

    Insersión de código faltante en CapaEntidades

--- CapaEntidades/DetalleVenta.cs
+++ CapaEntidades/DetalleVenta.cs
@@ -10,6 +10,24 @@ namespace CapaEntidades
 {
     public class DetalleVenta
     {
-       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DetalleVentaId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public decimal Precio { get; set; }
+
+        [Required]
+        public int ProductoId { get; set; }
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
+
+        [Required]
+        public int VentaId { get; set; }
+        [ForeignKey("VentaId")]
+        public Ventas Venta { get; set; }
+
+
     }
 }
