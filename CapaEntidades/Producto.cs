e61856d60921bdbc3b80fd6636d5da4f4eb97069
JonathanTej <tj0027032023@unab.edu.sv>
2024-04-11 12:07:07 -0600

    Modificación Producto

--- CapaEntidades/Producto.cs
+++ CapaEntidades/Producto.cs
@@ -11,6 +11,21 @@ namespace CapaEntidades
     public class Producto
     {
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdProducto { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(250)]
+        public string Descripcion { get; set; }
+        [Required]
+        public decimal PrecioUnitario { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+
 
     }
 }
