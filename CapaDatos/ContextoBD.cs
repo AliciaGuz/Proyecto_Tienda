1a685b000b8312b1ff715cced3761a965da13751
Xiomara1534 <cp0460032023@unab.edu.sv>
2024-04-11 21:17:14 -0600

    ComentariosContextoBD

--- CapaDatos/ContextoBD.cs
+++ CapaDatos/ContextoBD.cs
@@ -9,18 +9,27 @@ namespace CapaDatos
 {
     public partial class ContextoBD : DbContext
     {
+        // Constructor de la clase ContextoBD que configura la cadena de conexión.
         public ContextoBD()
             : base("name=ContextoBD")
         {
         }
 
-
+        // Método para configurar el modelo de datos.
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
         }
+
+        // Conjunto de entidades que representa la tabla de ventas en la base de datos.
         public virtual DbSet<Ventas> Ventas { get; set; }
+
+        // Conjunto de entidades que representa la tabla de productos en la base de datos.
         public virtual DbSet<Producto> Productos { get; set; }
+
+        // Conjunto de entidades que representa la tabla de detalles de venta en la base de datos.
         public virtual DbSet<DetalleVenta> DetalleVentas { get; set; }
+
+
 
     }
 }
