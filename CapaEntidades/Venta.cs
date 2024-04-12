4f7031f3e4f7fab70157cfdaebc583e75f8c24ed
Xiomara1534 <cp0460032023@unab.edu.sv>
2024-04-11 21:13:39 -0600

    ComentariosVentas

--- CapaEntidades/Venta.cs
+++ CapaEntidades/Venta.cs
@@ -10,16 +10,24 @@ namespace CapaEntidades
 {
     public class Ventas
     {
+        // Especifica que la propiedad VentaId es la clave primaria de la entidad.
         [Key]
+        // Especifica que el valor de VentaId será generado automáticamente por la base de datos.
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int VentaId { get; set; }
+
+        // Especifica que la propiedad Fecha es requerida (no puede ser nula).
         [Required]
         public DateTime Fecha { get; set; }
+
+        // Especifica que la propiedad Total es requerida (no puede ser nula).
         [Required]
         public decimal Total { get; set; }
 
-        // propiedad de navegación
+        // Propiedad de navegación que representa la relación entre Venta y DetalleVenta.
+        // Un objeto Venta puede tener una lista de objetos DetalleVenta asociados.
         public List<DetalleVenta> Detalles { get; set; }
+
 
     }
 }
